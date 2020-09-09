1ea54efc7e694afa177d1a01c609387150bbe9ea
shazena <shazena93@gmail.com>
2020-09-09 03:24:53 -0400

    Finished Forms for Dvd Library. Added in some pseudocode, planning for JS.

--- DDWA/Module003/003 DVD Library REST Client/js/home.js
+++ DDWA/Module003/003 DVD Library REST Client/js/home.js
@@ -1,3 +1,36 @@
 $(document).ready(function () {
+    //hide the add form
+    //hide the edit form
+    //on click of the search button, validate the inputs
+    //check to see if both a search term and category have been chosen
+    //if not, show the specific error
+    //how do you check to see the value of the select box?
+
+    //on click of the create button,
+    //hide the all tables form
+    //show the add form
+    //on click of the cancel button, empty values of all inputs and hide the form, and show the alldvds table
+    //on click of the create dvd button, run ajax call to add to server.
+    //after adding to the server, add the movie to the table and load the table. show the table of all dvds again.
+
+    //Q: is there any kind of validation that needs to be ran on the individual entries of the create dvd form?
+
+    //When you click on the edit button, it will hide the all dvds table
+    //then we will run the ajax call to the server to get the item by the id number
+    //and fill in the inputs of the edit form to match the results of the call
+    //then it will show the edit form
+
+    //on clicking the cancel button of the edit form, empty the values of all inputs
+    //hide the edit form
+    //show the all dvds table div
+
+    //on clicking the save changes button, run ajax call to put to server the new values
+    //probably run whatever validation first.
+    //you will need to make sure that the user did not enter something blank for the title. Check for whitespace.
+
+    //on clicking the delete button, get the id that is "hidden" in it.
+    //make the modal show up, but pass this hidden id to it.
+    //when you press the button in the modal, run the ajax call with the id to DELETE the dvd.
+    //on finishing the call, close the modal.
 
 });
