d17517ae72c4fbb13d30963d511a190c01d1d676
shazena <shazena93@gmail.com>
2020-09-11 00:40:57 -0400

    Making a backup before deleting everything and trying again

--- DDWA/Module003/003 DVD Library REST Client/js/home.js
+++ DDWA/Module003/003 DVD Library REST Client/js/home.js
@@ -1,9 +1,79 @@
+function validateSearchInput(category, searchTerm) {
+    if (category === null || searchTerm === "") {
+        $('#errors-show-all-table')
+            .append($('<li>')
+                .attr({class: 'list-group-item list-group-item-danger'})
+                .text('Both Search Category and Search Term are required.'));
+        return false;
+    } else return true;
+}
+
+function fetchSearchResults(category, searchTerm) {
+
+    var urlForApi = "https://tsg-dvds.herokuapp.com/dvds/" + category + "/" + searchTerm;
+
+    $.ajax({
+        type: 'GET',
+        URL: 'https://tsg-dvds.herokuapp.com/dvds/title/Minions',
+        success: function (dvd, status) {
+            alert(dvd[0]);
+            // $('#viewDvdTitle').html(data.title);
+            // $('#viewDvdYear').html(data.releaseYear);
+            // $('#viewDvdDirector').html(data.director);
+            // $('#viewDvdRating').html(data.rating);
+            // $('#viewDvdNotes').html(data.notes);
+
+
+            $('#showAllDvds').hide();
+            $('#viewDvdContainer').show();
+        },
+        error: function () {
+            $('#errors-show-all-table')
+                .append($('<li>')
+                    .attr({class: 'list-group-item list-group-item-danger'})
+                    .text('Error: That Dvd could not be found.'));
+        }
+    });
+
+}
+
+
 $(document).ready(function () {
     //hide the add form
     //hide the edit form
+    //hide show one dvd details
+    //hide the delete modal
+    $('#createDvd').hide();
+    $('#editDvd').hide();
+    $('#viewDvdContainer').hide();
+    $('#deleteOneDvdModal').hide();
     //on click of the search button, validate the inputs
     //check to see if both a search term and category have been chosen
     //if not, show the specific error
+    $('#search-button').click(function (event) {
+        $('#errors-show-all-table').empty();
+        //gather inputs, send to validation function
+        //if true, continue, if false, return false to end this process.
+        var category = $('#category-selection').val();
+        var searchTerm = $('#search-input').val();
+
+        //validation for search inputs
+        //if category is null, throw error
+        //if search term is a blank string, throw error.
+        if (!validateSearchInput(category, searchTerm)) {
+            return false;
+        }
+
+        fetchSearchResults(category, searchTerm);
+
+
+    });
+
+    $('#viewDvdBackButton').click(function (event) {
+        $('#viewDvdContainer').hide();
+        $('#showAllDvds').show();
+    })
+
     //how do you check to see the value of the select box?
 
     //on click of the create button,
@@ -30,11 +100,33 @@ $(document).ready(function () {
 
     //on clicking the delete button, get the id that is "hidden" in it.
     //make the modal show up, but pass this hidden id to it.
+    //DO NOT HIDE THE TABLE!!!!!
     //when you press the button in the modal, run the ajax call with the id to DELETE the dvd.
     //on finishing the call, close the modal.
+
+    // $('#exampleModal').on('show.bs.modal', function (event) {
+    //     var button = $(event.relatedTarget); // Button that triggered the modal
+    //     var recipient = button.data('whatever'); // Extract info from data-* attributes
+    //     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    //     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    //     var idToDelete = button.data('idNumber');
+    //     var modal = $(this);
+    //     $('#idToDeleteSpace').text(recipient);
+    //     // modal.find('.modal-title').text('New message to ' + recipient);
+    //     // modal.find('.modal-body input').val(recipient);
+    //
+    //
+    // });
+
 
     //on clicking the title of the movie
     //hide the showAllDvds table
     //then show the div for the oneDvd.
 
 });
+
+//error code from previous lessons:
+// $('#errorMessages')
+//     .append($('<li>')
+//         .attr({class: 'list-group-item list-group-item-danger'})
+//         .text('Error calling web service for Five Day Forecast. Please try again later.'));
