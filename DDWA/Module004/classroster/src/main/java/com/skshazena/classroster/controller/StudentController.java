68168c6a095646a315cdc808b5826f4b57daede5
shazena <shazena93@gmail.com>
2020-09-22 01:10:51 -0400

    Added Edit Student Validation

--- DDWA/Module004/classroster/src/main/java/com/skshazena/classroster/controller/StudentController.java
+++ DDWA/Module004/classroster/src/main/java/com/skshazena/classroster/controller/StudentController.java
@@ -5,9 +5,11 @@ import com.skshazena.classroster.dao.StudentDao;
 import com.skshazena.classroster.dao.TeacherDao;
 import com.skshazena.classroster.dto.Student;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -60,7 +62,10 @@ public class StudentController {
     }
 
     @PostMapping("editStudent")
-    public String performEditStudent(Student student) {
+    public String performEditStudent(@Valid Student student, BindingResult result) {
+        if (result.hasErrors()) {
+            return "editStudent";
+        }
         studentDao.updateStudent(student);
         return "redirect:/students";
     }
